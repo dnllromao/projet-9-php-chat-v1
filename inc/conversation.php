488adef8abc1bfdc0123d01282934d28fa815a79
dnllromao <dnllromao@gmail.com>
2017-08-21 15:57:49 +0200

    Convert smilies to images

--- inc/conversation.php
+++ inc/conversation.php
@@ -1,4 +1,13 @@
-<?php include 'pdo.php'; 
+<?php 
+
+	namespace Emojione; // remenber this php case
+
+	include 'pdo.php';
+	include __DIR__.'/emojione/php/autoload.php';
+
+	$client = new Client(new Ruleset());
+	$client->ascii = true;
+	$client->riskyMatchAscii = true;
 
 	$req = $db -> query("SELECT * FROM (SELECT msgs.id AS id, msgs.content AS content, HOUR(msgs.moment) AS hour, MINUTE(msgs.moment) AS minutes, users.pseudo AS user FROM msgs INNER JOIN users ON msgs.user_id = users.id ORDER BY msgs.id DESC LIMIT 10) AS new ORDER BY id ASC");
 
@@ -7,7 +16,9 @@
 	<!DOCTYPE html>
 	<html lang="en">
 	<head>
-		<meta http-equiv="refresh" content="5" url="<?= $_SERVER["HTTP_REFERER"];?>inc/conversation.php">
+		<meta http-equiv="refresh" content="5" url="<?= $_SERVER["HTTP_REFERER"];?>">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/emojione/2.2.7/assets/css/emojione.min.css">
+		
 	</head>
 	<body>
 		<ul class="inner-content">
@@ -18,7 +29,7 @@
 			<li>
 				<strong><?= $row['user']; ?></strong>
 				[<?= $row['hour']; ?>:<?= $row['minutes']; ?>]
-				<?= $row['content']; ?>
+				<?= $client->shortnameToImage($row['content']); ?>
 			</li>
 
 		<?php endwhile; ?>
--- inc/pdo.php
+++ inc/pdo.php
@@ -1,7 +1,7 @@
 <?php
 
 	try {
-		$db = new PDO('mysql:host=localhost;dbname=chat;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+		$db = new PDO('mysql:host=localhost;dbname=chat;charset=utf8', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 	} catch (Exception $e){
 		die('Error: '.$e->getMessage());
 	}
